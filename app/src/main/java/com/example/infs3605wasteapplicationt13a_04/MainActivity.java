b246ff8203e2538c770a29157dbe4dc626b12fa1
KaiwenLuo2003 <kwluo2003@gmail.com>
2023-10-14 22:10:47 +1100

    working maps api with zoomed map

--- app/src/main/java/com/example/infs3605wasteapplicationt13a_04/MainActivity.java
+++ app/src/main/java/com/example/infs3605wasteapplicationt13a_04/MainActivity.java
@@ -2,16 +2,32 @@ package com.example.infs3605wasteapplicationt13a_04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button showMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showMap = findViewById(R.id.showMap);
+
+        showMap.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //firebase documentation: https://firebase.google.com/docs/firestore/quickstart#java
         FirebaseFirestore db = FirebaseFirestore.getInstance();
