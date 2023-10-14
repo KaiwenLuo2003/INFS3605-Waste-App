3425bbd7cc0b89e5a85aff34fdead80816e36da8
ejlau <90582504+ejlau@users.noreply.github.com>
2023-10-14 17:36:37 +1100

    Added camera permission + intent to app

--- app/src/main/java/com/example/infs3605wasteapplicationt13a_04/MainActivity.java
+++ app/src/main/java/com/example/infs3605wasteapplicationt13a_04/MainActivity.java
@@ -1,12 +1,30 @@
 package com.example.infs3605wasteapplicationt13a_04;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    final static String TAG = "Main activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +34,50 @@ public class MainActivity extends AppCompatActivity {
         //firebase documentation: https://firebase.google.com/docs/firestore/quickstart#java
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+        Button signOutButton = findViewById(R.id.signOutButton);
+        Button cameraButton = findViewById(R.id.cameraButton);
+
+        ActivityResultLauncher<String> cameraPermission=registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if(result){
+                    Toast.makeText(MainActivity.this, "Camera Permission Granted", Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(MainActivity.this, "Full app functionality requires Camera Permission", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        cameraPermission.launch(android.Manifest.permission.CAMERA);
+
+        signOutButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+            }
+        });
+
+        cameraButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                try{
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                } catch (ActivityNotFoundException e){
+                    Log.d(TAG, "Camera not found");
+                }
+            }
+        });
+
     }
+
+
 }
