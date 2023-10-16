a822396586eccd87f6e24eb5219017a5a89b3453
KaiwenLuo2003 <kwluo2003@gmail.com>
2023-10-16 21:51:11 +1100

    updated with api pathing settext not working

--- app/src/main/java/com/example/infs3605wasteapplicationt13a_04/MainActivity.java
+++ app/src/main/java/com/example/infs3605wasteapplicationt13a_04/MainActivity.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     Button showMap;
+    Button getApi;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         showMap = findViewById(R.id.showMap);
+        getApi = findViewById(R.id.getApi);
+        textView = findViewById(R.id.textView);
+
+        textView.setText("testing");
+
 
         showMap.setOnClickListener(new View.OnClickListener() {
 
@@ -31,6 +40,18 @@ public class MainActivity extends AppCompatActivity {
 
         //firebase documentation: https://firebase.google.com/docs/firestore/quickstart#java
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        getApi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+                startActivity(intent);
+            }
+
+
+
+        });
+
 
     }
 }
