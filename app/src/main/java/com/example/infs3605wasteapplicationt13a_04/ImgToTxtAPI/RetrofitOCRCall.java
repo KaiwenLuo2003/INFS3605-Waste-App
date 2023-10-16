6962677a8c21897348a257030bab0805ce6b666d
ejlau <90582504+ejlau@users.noreply.github.com>
2023-10-16 22:51:00 +1100

    added TODOs

--- app/src/main/java/com/example/infs3605wasteapplicationt13a_04/ImgToTxtAPI/RetrofitOCRCall.java
+++ app/src/main/java/com/example/infs3605wasteapplicationt13a_04/ImgToTxtAPI/RetrofitOCRCall.java
@@ -10,4 +10,11 @@ public interface RetrofitOCRCall {
     @GET("https://app.nanonets.com/api/v2/OCR/Model/49289810-b2ea-4227-8e77-244ec6aec526")
     Call<ResponseBody> getModel();
 
+/*    TODO: try get POST method working
+      TODO: figure out how to access pic taken from camera
+      TODO: figure out how to get URL, pic details etc. from the phone
+      TODO: figure out how to send out the deets through POST method
+*/
+
+
 }
