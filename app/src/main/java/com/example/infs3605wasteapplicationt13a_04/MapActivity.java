54be6435fc431f10d5d19bc2b05b8f6b341d62ed
jacinda51 <jacindasuherly@gmail.com>
2023-10-28 21:25:21 +1100

    Map Markers

--- app/src/main/java/com/example/infs3605wasteapplicationt13a_04/MapActivity.java
+++ app/src/main/java/com/example/infs3605wasteapplicationt13a_04/MapActivity.java
@@ -14,28 +14,45 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    GoogleMap gMap;
-    FrameLayout map;
+    private GoogleMap mMap;
+
+    LatLng matravilleRecyclingCentre = new LatLng(-33.959420, 151.226710);
+    LatLng randwickRecyclingCentre = new LatLng(-33.911200, 151.243180);
+    LatLng NewCastle = new LatLng(-32.916668, 151.750000);
+    LatLng Brisbane = new LatLng(-27.470125, 153.021072);
+
+    private ArrayList<LatLng> locationArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        map = findViewById(R.id.map);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        locationArrayList = new ArrayList<>();
+
+
+        locationArrayList.add(matravilleRecyclingCentre);
+        locationArrayList.add(randwickRecyclingCentre);
     }
 
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        this.gMap = googleMap;
-        LatLng mapAus = new LatLng(-33.959420, 151.226710);
-        this.gMap.addMarker(new MarkerOptions().position(mapAus).title("Marker in Aus"));
-        this.gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mapAus, 16));
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        for (int i = 0; i < locationArrayList.size(); i++) {
+
+            mMap.addMarker(new MarkerOptions().position(locationArrayList.get(i)).title("Marker"));
+
+             mMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f));
+
+             mMap.moveCamera(CameraUpdateFactory.newLatLng(locationArrayList.get(i)));
+        }
     }
 }
